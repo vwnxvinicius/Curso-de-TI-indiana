f5ad50753148077e015481d287c7bcf672b329b9
vwnxvinicius <vwnx123@gmail.com>
2023-08-10 23:32:52 -0300

    Adicionada a classe Pessoa

---
+++ Modulo 2/OOP/Pessoa.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pessoa 
+{
+    public string $nome;
+    public int $idade;
+    public string $nacionalidade;
+    
+
+    public function setNome(string $nome): void 
+    {
+        $this->nome = $nome;
+    }
+
+    public function setIdade(int $idade): void 
+    {
+        $this->idade = $idade;
+    }
+
+    public function setNacionalidad(string $nacionalidade): void 
+    {
+        $this->nacionalidade = $nacionalidade;
+    }
+}
