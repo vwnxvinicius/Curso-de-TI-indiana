7bfb8eea196c6fe9fadad6930b921ca0f7a85170
vwnxvinicius <vwnx123@gmail.com>
2023-08-14 08:37:19 -0300

    Corrigida a implementação da classe Pessoa

--- Modulo_2/OOP/Exercicios/Pessoa.php
+++ Modulo_2/OOP/Exercicios/Pessoa.php
@@ -3,69 +3,20 @@
 class Pessoa
 {
     private string $nome;
-    public int $idade;
+    private int $idade;
     private string $corDosOlhos;
     private string $genero;
-    public float $altura; // Metro
-    public float $peso; // Kg
+    private float $altura;
+    private float $peso;
 
-    public function __construct(string $nome, string $corDosOlhos, string $genero)
+    public function __construct(string $nome, int $idade, string $corDosOlhos, string $genero, float $altura, float $peso)
     {
         $this->nome = $nome;
+        $this->setIdade($idade);
         $this->setCorDosOlhos($corDosOlhos);
         $this->setGenero($genero);
-    }
-
-    public function setIdade(int $idade): void
-    {
-        $this->idade = $idade;
-    }
-
-    private function setCorDosOlhos(string $corDosOlhos): void
-    {
-        switch ($corDosOlhos) {
-            case "Azul":
-                $this->corDosOlhos = $corDosOlhos;
-                break;
-            case "Castanho":
-                $this->corDosOlhos = $corDosOlhos;
-                break;
-            case "Verde":
-                $this->corDosOlhos = $corDosOlhos;
-                break;
-            case "Preto":
-                $this->corDosOlhos = $corDosOlhos;
-                break;
-            default:
-                echo "ERRO ao criar cor dos olhos -> Cor inválida, tente: Azul, Castanho, Verde, Preto" . PHP_EOL;
-                break;
-        }
-    }
-
-    private function setGenero(string $genero): void 
-    {
-        if ($genero == "Masculino") 
-        {
-            $this->genero = "Masculino";
-        } 
-        else if ($genero == "Feminino") 
-        {
-            $this->genero = "Feminino";
-        }
-        else 
-        {
-            echo "ERRO ao adicionar o gênero -> Gênero inválido, tente: Masculino ou Feminino" . PHP_EOL;;
-        }
-    }
-
-    public function setAltura(float $altura): void 
-    {
-        $this->altura = $altura;
-    }
-
-    public function setPeso(float $peso): void 
-    {
-        $this->peso = $peso; 
+        $this->setAltura($altura);
+        $this->setPeso($peso);
     }
 
     public function getNome(): string
@@ -73,27 +24,88 @@ class Pessoa
         return $this->nome;
     }
 
-    public function getCorDosOlhos(): string 
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    private function setIdade(int $idade): void
+    {
+        if (($idade < 0) || ($idade > 100)) {
+            echo "Idade informada é invalida" . PHP_EOL;
+            exit();
+        }
+
+        $this->idade = $idade;
+    }
+
+    private function setCorDosOlhos(string $corDosOlhos): void
+    {
+        $opcoes = ["Azul", "Castanho", "Verde", "Preto", "Rosa"];
+
+        if (in_array($corDosOlhos, $opcoes)) {
+            $this->corDosOlhos = $corDosOlhos;
+            return;
+        }
+
+        echo "Cor dos olhos inválida" . PHP_EOL;
+        exit();
+    }
+
+    public function getCorDosOlhos(): string
     {
         return $this->corDosOlhos;
     }
 
-    public function getGenero(): string 
+    private function setGenero(string $genero): void
+    {
+        $opcoes = ["Masculino", "Feminino"];
+
+        if (in_array($genero, $opcoes)) {
+            $this->genero = $genero;
+            return;
+        }
+
+        echo "Genero inválido" . PHP_EOL;
+        exit();
+    }
+
+    public function getGenero(): string
     {
         return $this->genero;
     }
 
+
+    private function setAltura(float $altura): void 
+    {
+        if (($altura < 0) || ($altura >=2.20)) 
+        {
+            echo "Altura inválida";
+            exit();
+        }
+
+        $this->altura = $altura;
+    }
+
+    public function getAltura(): float
+    {
+        return $this->altura;
+    }
+
+    private function setPeso(float $peso): void 
+    {
+        if ($peso < 0)
+        {
+            echo "Peso inválido!";
+            exit();
+        }
+
+        $this->peso = $peso;
+    }
+    
+
+    public function getPeso(): float
+    {
+        return $this->peso;
+    }
 }
-
-$pessoa = new Pessoa("Vinicius", "Castanho", "Masculino");
-$pessoa->setAltura(1.68);
-$pessoa->setIdade(18);
-$pessoa->setPeso(63);
-
-echo $pessoa->getNome() . PHP_EOL;
-echo $pessoa->getCorDosOlhos() . PHP_EOL;
-echo $pessoa->getGenero() . PHP_EOL;
-
-echo $pessoa->idade . PHP_EOL;
-echo $pessoa->altura . PHP_EOL;
-echo $pessoa->peso . PHP_EOL;
---
+++ Modulo_2/OOP/Exercicios/teste.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Pessoa.php';
+
+$pessoaValida = new Pessoa('Jose', 20, 'Azul', 'Masculino', 1.68, 70);
+
+var_dump($pessoaValida);
+//var_dump($pessoaInvalida);
